a8a9d82436e2c1dfb94ccc886156c192217d3fcd
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-10-20 20:08:54 +0000

    Update task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS Books (
 );
 
 -- Create the Customers table
-CREATE TABLE IF NOT EXISTS Customers (
+CREATE TABLE IF NOT EXISTS Customer (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL,
