89f4b38f3aecec50cdba63280fc7dfa06b1b2761
fouad-95m <78822171+fouad-95m@users.noreply.github.com>
2024-10-20 19:52:06 +0000

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,6 @@
 -- Query to print full description of the 'books' table
 SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+  ["COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books'"]
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = DATABASE() -- Automatically refers to the current database
 AND TABLE_NAME = 'books';
